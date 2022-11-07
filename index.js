9a8943fd5d4c32ac8aea89c51bb4ddd68a8f707c
MetaMoniker <metamoniker@gmail.com>
2022-11-06 22:58:36 -0500

    updated info

--- index.js
+++ index.js
@@ -100,7 +100,8 @@ qs(`.p1`).innerHTML = `<p><i>behind the scenes...</i></p>`;
 
 qs(
   `.p2`
-).innerHTML = `<p>Parts of the equation within a pair of brackets (stacks) are treated as independent equations, solved, and re-embedded into the larger equation. Repeat until no stacks left and final answer displayed.<br><br>... css to make it pretty!</p>`;
+).innerHTML = `<p>▸ paired brackets create (stack)<br>▸ solved indpendently<br>▸ substituted into equation<br>▸ equation re-parsed <br>▸ repeat until no stacks left<br>
+✔ final answer displayed!<br><br>... css to make it pretty!</p>`;
 
 function overlay(io) {
   if (io) {
